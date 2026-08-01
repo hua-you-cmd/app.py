df357158990510ca02bd30e7b811ce783491effa
hua-you-cmd <huashenfo@gmail.com>
2026-08-01 17:32:36 +0900

    Remove blank line in KPI card section
    Remove unnecessary blank line before KPI card highlight.

--- tokeyi.py
+++ tokeyi.py
@@ -680,7 +680,6 @@ st.info(f"🕒 最終データ更新日時: **{last_time}** (Yahoo Finance リ�
 
 # 8. ハイライト KPI カード
 
-    
 if df_res is not None and len(df_res) > 0:
     top_pair = df_res[0] # .iloc を取って [0] だけにする
     st.write(f"注目ペア: {top_pair['display_name']}")
